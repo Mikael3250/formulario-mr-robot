5afa9b4b1caa60c845b5575619af3882ff4cfcc6
Mikael Bondan <122181666+Mikael3250@users.noreply.github.com>
2023-10-15 19:12:50 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+/* 
+era pra ter um código sensacional de JavaScript aqui
+ */
+
+/*
+não consegui deixar do jeito que eu queria, quando apenas todos os inputs tivessem sido preenchidos o alert box com a mensagem fosse enviado :(
+e tbm ao apertar o botão sair barulinho de plim plim <3
+deixo abaixo uma das tentativas!
+\/\/\/\/\/\/\/\/\/
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("botao-enviar").addEventListener("click", function() {
+        alert("Suas informações foram enviadas ao nosso banco de dados com sucesso!!!");
+    });
+});
+
+*/
